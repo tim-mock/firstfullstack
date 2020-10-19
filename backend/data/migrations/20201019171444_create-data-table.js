e8a5f5988423b83ef8885e2e541f0d64598cc302
tim <timmock@tims-mbp.lan>
2020-10-19 17:54:03 -0400

    just in case babey!

---
+++ backend/data/migrations/20201019171444_create-data-table.js
@@ -0,0 +1,38 @@
+module.exports.up = function (knex) {
+  return knex.schema
+    .createTable("users", (tbl) => {
+      tbl.increments();
+      tbl.string("username", 128).notNullable().unique();
+    })
+    .createTable("todolist", (tbl) => {
+      tbl.increments();
+      tbl.string("task", 128).notNullable();
+      tbl.boolean("completed");
+      tbl
+        .integer("user")
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    })
+    .createTable("groceries", (tbl) => {
+      tbl.increments();
+      tbl.string("item", 128).notNullable();
+      tbl.boolean("purchased");
+      tbl
+        .integer("user")
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    });
+};
+
+module.exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("groceries")
+    .dropTableIfExists("todolist")
+    .dropTableIfExists("users");
+};
